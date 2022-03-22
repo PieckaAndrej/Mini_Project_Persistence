a5001f7fe753c79bf1d697ed1e87e4b433e2c62a
Goorson <10407543@ucn.dk>
2022-03-22 10:27:07 +0100

    Update Customer.java

--- src/model/Customer.java
+++ src/model/Customer.java
@@ -1,5 +1,71 @@
 package model;
 
 public class Customer {
+	private String name;
+	private String address;
+	private String city;
+	private String phone;
+	private int zipcode;
+	
+	
+	public Customer(String name, String address, String city, String phone, int zipcode) {
+		super();
+		this.name = name;
+		this.address = address;
+		this.city = city;
+		this.phone = phone;
+		this.zipcode = zipcode;
+	}
 
+
+	public String getName() {
+		return name;
+	}
+
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+
+	public String getAddress() {
+		return address;
+	}
+
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+
+	public String getCity() {
+		return city;
+	}
+
+
+	public void setCity(String city) {
+		this.city = city;
+	}
+
+
+	public String getPhone() {
+		return phone;
+	}
+
+
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+
+
+	public int getZipcode() {
+		return zipcode;
+	}
+
+
+	public void setZipcode(int zipcode) {
+		this.zipcode = zipcode;
+	}
+	
+	
 }
