7effe0a3aeb649a755d5b72a77d43743075ef681
CosminRaita <93519047+CosminRaita@users.noreply.github.com>
2022-03-22 11:11:07 +0100

    connection test

---
+++ src/test/DatabaseConnectionTest.java
@@ -0,0 +1,29 @@
+package test;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.sql.Connection;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import database.DbConnection;
+
+public class DatabaseConnectionTest {
+
+	DbConnection db = null;
+	Connection con = null;
+
+	@BeforeEach
+	public void setUp() {
+		con = DbConnection.getInstance().getConnection(0);
+	}
+	
+	
+	@Test
+	public void wasConnected() {
+		assertNotNull(con, "Connected - connection cannot be null");	
+	}
+
+}
