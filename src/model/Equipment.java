1cb670d37c2393c5cf748de405fa4c3952b4dd53
Goorson <10407543@ucn.dk>
2022-03-23 10:00:38 +0100

    more

--- src/model/Equipment.java
+++ src/model/Equipment.java
@@ -1,5 +1,15 @@
 package model;
 
-public class Equipment {
+public class Equipment extends Product {
+
+	private String type;
+	private String description;
+	
+	public Equipment(String id, String name, double puchasePrice, double salesPrice, String countryOfOrigin,
+			int minStock, String type, String description) {
+		super(id, name, puchasePrice, salesPrice, countryOfOrigin, minStock);
+		this.type = type;
+		this.description = description;
+	}
 
 }
--- src/model/Product.java
+++ src/model/Product.java
@@ -1,5 +1,24 @@
 package model;
 
 public class Product {
-
+	
+	private String id;
+	private String name;
+	private double puchasePrice;
+	private double salesPrice;
+	private String countryOfOrigin;
+	private int minStock;
+	
+	public Product(String id, String name, double puchasePrice, double salesPrice, String countryOfOrigin,
+			int minStock) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.puchasePrice = puchasePrice;
+		this.salesPrice = salesPrice;
+		this.countryOfOrigin = countryOfOrigin;
+		this.minStock = minStock;
+	}
+	
+	
 }
