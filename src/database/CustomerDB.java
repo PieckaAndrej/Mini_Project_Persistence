de5283dd79bb47d4807c764670a3633444b70369
Goorson <10407543@ucn.dk>
2022-03-23 09:45:04 +0100

    push DBIF

--- src/database/CustomerDB.java
+++ src/database/CustomerDB.java
@@ -1,5 +1,33 @@
 package database;
 
-public class CustomerDB {
+import model.Customer;
+
+public class CustomerDB implements CustomerDBIF {
+	
+
+	@Override
+	public Customer create(Customer c) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean delete(Customer c) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public Customer update(Customer c) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+
+	@Override
+	public Customer getCustomerByPhone(String phone, boolean fullAssociation) {
+		
+		return null;
+	}
 
 }
--- src/database/CustomerDBIF.java
+++ src/database/CustomerDBIF.java
@@ -1,5 +1,11 @@
 package database;
 
-public class CustomerDBIF {
+import model.Customer;
 
+public interface CustomerDBIF {
+
+	public Customer insert(Customer c);
+	public boolean delete(Customer c);
+	public Customer update(Customer c);
+	public Customer getCustomerByPhone(String phone, boolean fullAssociation);
 }
