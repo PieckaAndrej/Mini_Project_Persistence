47426668acd5e929965e54218e639d41a10d6a7f
BarnabyUCN <10407536@ucn.dk>
2022-03-23 11:41:50 +0100

    asd

--- src/controller/SaleOrderController.java
+++ src/controller/SaleOrderController.java
@@ -2,16 +2,21 @@ package controller;
 
 import model.SaleOrder;
 import model.OrderLine;
+import dal.SaleOrderDBIF;
+import dal.SaleOrderDB;
 
 public class SaleOrderController {
-	
+	private SaleOrderDBIF saleOrderDatabase;
+	private PersonController personController;
+	private SaleOrder currentOrder;
 	
 	public SaleOrderController() {
-		
+		saleOrderDatabase = new SaleOrderDB();
+		personController = new PersonController();
 	}
 	
-	public SaleOrder createSaleOrder(String date, String deliveryStatus, String deliveryDate) {
-		return null;
+	public SaleOrder createOrder(String phone) {
+		currentOrder = new SaleOrder(personController.getPersonByPhone(phone));
 	}
 	public SaleOrder addProduct(int id, int amount) {
 		return null;
