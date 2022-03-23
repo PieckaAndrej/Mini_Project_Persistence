c9d90a0c101878033e48aab3f6f0aea7c9d71bcb
BarnabyUCN <10407536@ucn.dk>
2022-03-23 11:02:59 +0100

    asd1

---
+++ src/controller/SaleOrderController.java
@@ -0,0 +1,19 @@
+package controller;
+import database.SaleOrderDb;
+import model.SaleOrder;
+import model.OrderLine;
+
+public class SaleOrderController {
+	public SaleOrderController() {
+		public SaleOrder createSaleOrder(String date, String deliveryStatus, String deliveryDate) {
+			
+		}
+		public SaleOrder addProduct(int id, int amount) {
+			
+		}
+		public finishOrder() {
+			
+		}
+	}
+	
+}
--- src/model/Copy.java
+++ src/model/Copy.java
@@ -1,5 +1,33 @@
 package model;
+import java.time.LocalDate;
 
 public class Copy {
-
+	
+	private int copyID;
+	private boolean inRentable;
+	private LocalDate rentDate;
+	private Product product;
+	
+	//Constructor for Copy
+	public Copy(int copyID, boolean inRentable, LocalDate rentDate) {
+		this.copyID = copyID;
+		inRentable = true;
+		this.rentDate = rentDate;
+	}
+	//Returns the price of the copy
+	public double getRentPrice() {
+	return rentPrice();
+	}
+	//Sets the price of the copy
+	public void setRentPrice(double rentPrice) {
+		this.rentPrice = rentPrice;
+	}
+	//Checks if the copy is rentable
+	public boolean inRentable() {
+		return inRentable;
+	}
+	//Sets the copy to rentable or not
+	public void setInRentable(boolean inRentable) {
+		this.inRentable = inRentable;
+	}
 }
