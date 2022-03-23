3a8841234f0582c2e6386f53c6ba5e24316ff3b4
Goorson <10407543@ucn.dk>
2022-03-23 10:37:18 +0100

    dal = database

--- src/database/ProductDB.java
+++ src/database/ProductDB.java
@@ -6,6 +6,7 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 
+import dal.DbConnection;
 import model.Product;
 
 public class ProductDB implements ProductDBIF {
