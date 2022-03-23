c12efda56dafec4c815cdbaf386fefb1ec550d62
CosminRaita <93519047+CosminRaita@users.noreply.github.com>
2022-03-23 11:17:26 +0100

    PersonController

--- src/controller/PersonController.java
+++ src/controller/PersonController.java
@@ -1,5 +1,19 @@
 package controller;
 
-public class PersonController {
+import dal.PersonDB;
+import dal.PersonDBIF;
+import model.Person;
 
+public class PersonController {
+	private PersonDBIF personDb;
+	
+	public PersonController()
+	{
+		personDb = new PersonDB();
+	}
+	
+	public Person getCustomerByPhone(String phone)
+	{
+		return personDb.getCustomerByPhone(phone);
+	}
 }
