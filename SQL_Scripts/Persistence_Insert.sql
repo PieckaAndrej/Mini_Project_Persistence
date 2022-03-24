fbb63eb91ffcd927815c2ab4dfd0a5dfa4069d56
AzizKadem <93520332+AzizKadem@users.noreply.github.com>
2022-03-24 20:07:39 +0100

    sql insert added

---
+++ SQL_Scripts/Persistence_Insert.sql
@@ -0,0 +1,21 @@
+use [CSC-CSD-S211_10407533]
+
+insert into Country values('Denmark','9000','Aalborg');
+
+go
+
+insert into Person values('Joe','Norgesgade 18 2tv','Denmark','9000','1234567890', 'joe@gmail.com');
+
+go
+
+insert into Product values('hat',10,15.00015,'USA',2,8,'1234567890');
+
+insert into Product values('AK-47',10,1500,'Vietnam',2,20,'1234567890');
+
+insert into Product values('Cowboy T-Shirt',10,5,'Denmark',4,100,'1234567890');
+
+go
+
+insert into GunReplica values(7.62, 'metal', 2);
+
+insert into Clothing values('XL', 'blue', 3);
